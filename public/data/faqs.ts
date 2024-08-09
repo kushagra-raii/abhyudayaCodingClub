67bd1028947324170b3b81741321d703eb9cac52
kushagra-raii <kushagrarai83@gmail.com>
2024-08-09 13:28:36 +0530

    dataUpdated

--- public/data/faqs.ts
+++ public/data/faqs.ts
@@ -2,7 +2,7 @@ export const faqs: Array<{ id: number; ques: string; desc: string }> = [
   {
     id: 1,
     ques: "What are the activities?",
-    desc: "Activity like Workshop, mentor session and hackathon will be conducted  in under the Abhyudaya club dgsdgsdg  af ad fsgsd gsdsdsdg  dsgdgsdsd gdgsd gdds gdsg dsgsdgdg bsjdg kgkj jkghhjhkdjhdjhgkj hsdjk hjk hds ds jhdsbd jbk dskjkjsdj jdgkjdsnjgdjngd jnsj j n j jb jbgdjb gd d jb s jbs bjbsjb jb  bj dgskjb  gdskjbd gkjb jdgjk gjb d jbgsdjbds jbbjsg  bjkdb sjdjsjb bjd",
+    desc: "Activity like Workshop, mentor session and hackathon will be conducted  in under the Abhyudaya club.",
   },
   {
     id: 2,
